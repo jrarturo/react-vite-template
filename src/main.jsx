4a74ef7a3681e77445b5216500cde60da38cf07a
Alvaro Arturo <alvaroj.arturo@gmail.com>
2022-06-13 16:48:16 -0500

    First Vite-React project

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import '././index.css'
+
+
+const app = <h1>dasda <img src='/logo.svg' /></h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(app);
